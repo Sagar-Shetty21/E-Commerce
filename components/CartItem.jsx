116c040969c40fa734bc62e46d49b5b4ed4f6101
sagar <5agarm5hetty05@gmail.com>
2023-10-03 10:45:44 +0530

    modified cart item

--- components/CartItem.jsx
+++ components/CartItem.jsx
@@ -1,14 +1,18 @@
 import { CartContext } from "@context/CartContext";
 import { useContext, useState } from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import Button from "./Button";
 
+const CartItemCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  box-shadow: 4px 4px 8px rgba(0, 0, 0, 0.2);
+`
 
 const CartItemContainer = styled.div`
   display: flex;
-  padding: 0.625rem 0; /* py-5 */
+  padding: 0.625rem 0.4rem; /* py-5 */
   gap: 0.375rem; /* gap-3 */
-  border-bottom: 1px solid #cbcbcbbf; /* border-b */
 
   @media (min-width: 768px) {
     gap: 1.25rem; /* md:gap-5 */
@@ -157,60 +161,90 @@ const SelectorsContainer = styled.div`
   display: flex;
   gap: 20px;
 `;
+const ExpandButton = styled.div`
+  display: flex;
+  justify-content: center;
+  opacity: 0.6;
+  border-top: 1px solid #cbcbcbbf;
+  svg{
+    height: 18px;
+  }
+  &:hover{
+    opacity: 1;
+  }
+`;
+const MoreProductInfo = styled.div`
+  height: 0;
+  overflow: hidden;
+  transition: height 0.8s ease;
+  ${props => props.expandCard && css`
+    height: auto;
+  `}
+`;
+
+const MoreInfoContainer = styled.div`
+  
+`
 
 // Usage example in your component
 const CartItem = ({item, quantity}) => {
   const {removeProduct, addToCart} = useContext(CartContext)
+  const [expandCard, setExpandCard] = useState(false)
   
   return (
-    <CartItemContainer>
-      <ProductImageContainer>
-        <ProductImage
-          src={item?.images[0]}
-          alt="product image"
-        />
-      </ProductImageContainer>
-      <ProductInfoContainer>
-        <ProductNamePriceRow>
-          <ProductTitle>{item.title}</ProductTitle>
-          <ProductPrice>MRP : &#8377;{quantity*item.price}</ProductPrice>
-        </ProductNamePriceRow>
-        <SizeQuantityContainer>
-          <SelectorsContainer>
-            <StyledSelector>
-              <div>Size:</div>
-              <SelectSize /* onChange={(e) => updateCartItem(e, 'selectedSize')} */>
-                <option vlaue="2x2">2 x 2</option>
-                <option vlaue="2x9">2 x 9</option>
-                <option vlaue="2x9">2 x 9</option>
-                <option vlaue="2x9">2 x 9</option>
-                <option vlaue="2x9">2 x 9</option>
-                <option vlaue="2x9">2 x 9</option>
-              </SelectSize>
-            </StyledSelector>
-            <StyledSelector>
-              <div>Quantity:</div>
-              <Button onClick={() => removeProduct(item._id)}>
-                <svg style={{margin: '0'}} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg>
-              </Button>
-              <SelectQuantity value={quantity} type="number" /* onChange={(e) => updateCartItem(e, 'quantity')} */ />
-              <Button onClick={() => addToCart(item._id)}>
-                <svg style={{margin: '0'}} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg>
-              </Button>
-            </StyledSelector>
-          </SelectorsContainer>
-          <DeleteIcon onClick={() => removeProduct(item._id)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-          </DeleteIcon>
-        </SizeQuantityContainer>
-      </ProductInfoContainer>
-
-
-    </CartItemContainer>
+    <CartItemCard>
+      <CartItemContainer>
+        <ProductImageContainer>
+          <ProductImage
+            src={item?.images[0]}
+            alt="product image"
+          />
+        </ProductImageContainer>
+        <ProductInfoContainer>
+          <ProductNamePriceRow>
+            <ProductTitle>{item.title}</ProductTitle>
+            <ProductPrice>MRP : &#8377;{quantity*item.price}</ProductPrice>
+          </ProductNamePriceRow>
+          <SizeQuantityContainer>
+            <SelectorsContainer>
+              <StyledSelector>
+                <div>Quantity:</div>
+                <Button onClick={() => removeProduct(item._id)}>
+                  <svg style={{margin: '0'}} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                  </svg>
+                </Button>
+                <SelectQuantity value={quantity} type="number" /* onChange={(e) => updateCartItem(e, 'quantity')} */ />
+                <Button onClick={() => addToCart(item._id)}>
+                  <svg style={{margin: '0'}} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                  </svg>
+                </Button>
+              </StyledSelector>
+            </SelectorsContainer>
+            <DeleteIcon onClick={() => removeProduct(item._id)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+            </DeleteIcon>
+          </SizeQuantityContainer>
+        </ProductInfoContainer>
+      </CartItemContainer>
+      <MoreInfoContainer>
+        {expandCard && 
+          <MoreProductInfo expandCard={expandCard}>jhgfd</MoreProductInfo>
+        }
+        <ExpandButton onClick={() => setExpandCard(!expandCard)} >
+          {expandCard ?
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+              <path fill-rule="evenodd" d="M11.47 7.72a.75.75 0 011.06 0l7.5 7.5a.75.75 0 11-1.06 1.06L12 9.31l-6.97 6.97a.75.75 0 01-1.06-1.06l7.5-7.5z" clip-rule="evenodd" />
+            </svg>          
+          :
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+              <path fill-rule="evenodd" d="M12.53 16.28a.75.75 0 01-1.06 0l-7.5-7.5a.75.75 0 011.06-1.06L12 14.69l6.97-6.97a.75.75 0 111.06 1.06l-7.5 7.5z" clip-rule="evenodd" />
+            </svg>
+          }
+        </ExpandButton>
+      </MoreInfoContainer>
+    </CartItemCard>
   );
 };
 
